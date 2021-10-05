5e967deb5302109da18307be9ca31535799d40b3
Samuel I.J <samueljordan322@gmail.com>
2021-10-05 17:23:29 +0700

    Polishing main.go

--- main.go
+++ main.go
@@ -13,36 +13,6 @@ func main() {
 	config.InitLog()
 	config.InitMigration()
 
-	// fmt.Println("Calling API...")
-
-	// result := lib.GetWord("en", "hello")
-
-	// fmt.Println(result)
-	// fmt.Println(hello.Meanings[1])
-	// fmt.Println(hello.Phonetics[1])
-
-	// contoh kode 2
-	// client := &http.Client{}
-	// req, err := http.NewRequest("GET", "https://api.dictionaryapi.dev/api/v2/entries/en/hello", nil)
-	// if err != nil {
-	// 	fmt.Print(err.Error())
-	// }
-	// req.Header.Add("Accept", "application/json")
-	// req.Header.Add("Content-Type", "application/json")
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	fmt.Print(err.Error())
-	// }
-	// defer resp.Body.Close()
-	// bodyBytes, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	fmt.Print(err.Error())
-	// }
-
-	// var responseObject dictionary
-	// json.Unmarshal(bodyBytes, &responseObject)
-	// fmt.Printf("API Response as struct %+v\n", responseObject)
-
 	e := echo.New()
 	e.Use(middleware.Log)
 	route.NewUser(e)
@@ -51,5 +21,5 @@ func main() {
 	route.NewReminders(e)
 	route.NewPictures(e)
 	route.Dict(e)
-	e.Start(":8080")
+	panic(e.Start(":8080"))
 }
